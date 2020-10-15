0390e10004b45e2b62895827a9221a0911bbd542
Vinicius <pauloviniciusrp9@gmail.com>
2020-10-15 00:10:02 -0300

    [MOD] Save item in DB

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,39 @@
-import express from 'express'
+import express, { request, response } from 'express'
+import { getRepository } from 'typeorm'
+import Orphanages from './models/Orphanage'
 
 import './database/connection'
 
 const app = express()
 
 app.use(express.json())
+
+app.post('/orphanages', async (request, response) => {
+  const {
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  } = request.body
+
+  const orphanagesRepository = getRepository(Orphanages)
+
+  const orphanages = orphanagesRepository.create({
+    name,
+    latitude,
+    longitude,
+    about,
+    instructions,
+    opening_hours,
+    open_on_weekends
+  })
+
+  await orphanagesRepository.save(orphanages)
+
+  return response.json({ message: 'Hello World' })
+})
 
 app.listen(3333)
