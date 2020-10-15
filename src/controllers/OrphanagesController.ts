4ea85e2142d514e6eba6e9e752a2bb826e0dd9a7
Vinicius <pauloviniciusrp9@gmail.com>
2020-10-15 17:03:13 -0300

    commit

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express'
 import { getRepository } from 'typeorm'
 import orphanageView from '../views/orphanages_view'
+import * as Yup from 'yup'
 
 import Orphanages from '../models/Orphanage'
 
 export default {
+    // -------------------- List Orphanages --------------------
     async index(request: Request, response: Response) {
         const orphanagesRepository = getRepository(Orphanages)
 
@@ -14,6 +16,8 @@ export default {
 
         return response.json(orphanageView.renderMany(orphanages))
     },
+
+    // -------------------- List Orphanage --------------------
     async show(request: Request, response: Response) {
         const { id } = request.params
 
@@ -26,6 +30,7 @@ export default {
         return response.json(orphanageView.render(orphanage))
     },
 
+    // -------------------- CREATE --------------------
     async create(request: Request, response: Response) {
         const {
             name,
@@ -41,10 +46,10 @@ export default {
 
         const requestImages = request.files as Express.Multer.File[]
         const images = requestImages.map(image => {
-            return { path: image.filename}
+            return { path: image.filename }
         })
 
-        const orphanages = orphanagesRepository.create({
+        const data = {
             name,
             latitude,
             longitude,
@@ -53,7 +58,26 @@ export default {
             opening_hours,
             open_on_weekends,
             images
+        }
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(Yup.object().shape({
+                path: Yup.string().required()
+            }))
         })
+
+        await schema.validate(data, {
+            abortEarly: false
+        })
+
+        const orphanages = orphanagesRepository.create(data)
 
         await orphanagesRepository.save(orphanages)
 
